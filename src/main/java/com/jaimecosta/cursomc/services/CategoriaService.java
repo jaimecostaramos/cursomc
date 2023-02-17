73cfaada5e84bd42002c04a5a11d701597bc16c0
Jaime Costa <jaimecostaramos@gmail.com>
2023-02-17 14:45:38 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/jaimecosta/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.jaimecosta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jaimecosta.cursomc.domain.Categoria;
+import com.jaimecosta.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+
+	}
+}
